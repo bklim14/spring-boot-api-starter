6f035864900d714f52c6abac625fbcacc7ecceff
Brian Kent Lim <brian.lim.14@gmail.com>
2018-07-17 09:45:56 +0800

    Added find all novena

--- src/main/java/io/bklim/novena/NovenaService.java
+++ src/main/java/io/bklim/novena/NovenaService.java
@@ -1,5 +1,8 @@
 package io.bklim.novena;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,14 @@ public class NovenaService {
 	
 	public void addNovena(Novena novena) {
 		novenaRepository.save(novena);
+	}
+	
+	public List<Novena> getNovenaList() {
+		
+		List<Novena> novenaList = new ArrayList<>();
+		novenaRepository.findAll().forEach(novena -> novenaList.add(novena));
+		
+		return novenaList;
 	}
 
 }
